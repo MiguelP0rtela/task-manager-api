bcf48d8232df8023b5210e368f0de7c524ca66a2
HelloW0rld <mp.migueel@gmail.com>
2026-08-01 14:12:33 +0100

    Set up Alembic for database migrations.

--- app/database/init_db.py
+++
@@ -1,7 +0,0 @@
-from app.database.database import Base, engine
-from app.models.user import User
-from app.models.task import Task
-
-Base.metadata.create_all(bind=engine)
-
-print("Base de dados inicializada com sucesso!")
