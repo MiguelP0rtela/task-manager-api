29f8731e0a7aa4f2dca73bb8b065a11e5155712a
HelloW0rld <mp.migueel@gmail.com>
2026-08-03 18:28:38 +0100

    test: add user endpoint tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,3 @@
-from idlelib.rpc import response_queue
-
-
 def test_health(client):
     response = client.get("/health")
 
@@ -55,3 +52,88 @@ def test_no_data(client):
     assert response.status_code == 422
 
 
+def test_password_too_short(client):
+    user = {
+        "username": "miguel",
+        "email": "miguel@gmail.com",
+        "password": "Test12!"
+    }
+
+    response = client.post("/users", json=user)
+
+    assert response.status_code == 422
+
+    errors = response.json()["detail"]
+
+    assert any(
+        error["loc"][-1] == "password" and "at least 8 characters" in error["msg"].lower()
+        for error in errors
+    )
+
+
+def test_password_without_uppercase_letter(client):
+    user = {
+        "username": "miguel",
+        "email": "miguel@gmail.com",
+        "password": "test123!"
+    }
+    response = client.post("/users", json=user)
+
+    assert response.status_code == 422
+    errors = response.json()["detail"]
+
+    assert any(
+        error["loc"][-1] == "password" and "one uppercase letter" in error["msg"].lower()
+        for error in errors
+    )
+
+
+def test_password_without_lowercase_letter(client):
+    user = {
+        "username": "miguel",
+        "email": "miguel@gmail.com",
+        "password": "TEST123!"
+    }
+    response = client.post("/users", json=user)
+
+    assert response.status_code == 422
+    errors = response.json()["detail"]
+
+    assert any(
+        error["loc"][-1] == "password" and "one lowercase letter" in error["msg"].lower()
+        for error in errors
+    )
+
+
+def test_password_without_number(client):
+    user = {
+        "username": "miguel",
+        "email": "miguel@gmail.com",
+        "password": "Teste!@#%!"
+    }
+    response = client.post("/users", json=user)
+
+    assert response.status_code == 422
+    errors = response.json()["detail"]
+
+    assert any(
+        error["loc"][-1] == "password" and "one number" in error["msg"].lower()
+        for error in errors
+    )
+
+
+def test_password_without_special_characters(client):
+    user = {
+        "username": "miguel",
+        "email": "miguel@gmail.com",
+        "password": "Teste12345"
+    }
+    response = client.post("/users", json=user)
+
+    assert response.status_code == 422
+    errors = response.json()["detail"]
+
+    assert any(
+        error["loc"][-1] == "password" and "one special character" in error["msg"].lower()
+        for error in errors
+    )
